6b54fe0500ec4089da7a64840e2c05281a77d6c1
Adam Conn <adamc@uniform.dev>
2022-03-28 16:43:42 -0700

    activated context

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
-import "../styles/globals.css";
-import "../styles/page.css";
+import { UniformContext } from '@uniformdev/context-react';
+import { Context } from '@uniformdev/context';
+
+import manifest from '../lib/contextManifest.json';
+
+import '../styles/globals.css';
+import '../styles/page.css';
+
+const context = new Context({
+  manifest,
+  defaultConsent: true,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <UniformContext context={context}>
+      <Component {...pageProps} />
+    </UniformContext>
+  );
 }
 
 export default MyApp;
